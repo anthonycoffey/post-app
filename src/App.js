1352d395303ef5ca289735723d89d609ec5e5c3d
Anthony Coffey <coffey.j.anthony@gmail.com>
2020-12-21 19:11:37 -0600

    adding loading spinner

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import { connect } from "react-redux";
 import Player from "./components/Player";
 import FooterNav from "./components/FooterNav/FooterNav";
 import HeaderNav from "./components/HeaderNav/HeaderNav";
+import Loader from "react-loader-spinner";
 import { getCourseRequest } from "./store/actions/course.action";
 
 const App = ({ course, getCourse }) => {
   const [chapterIndex, setChapterIndex] = useState(-1);
   const [pageIndex, setPageIndex] = useState(0);
   const [headerTitle, setHeaderTitle] = useState("Main Menu");
+  const [isLoading, setLoadingStatus] = useState(true);
+
   let totalPageCount = 0;
   let totalChapterCount = 0;
   useEffect(() => {
     getCourse();
+    setLoadingStatus(false);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   const handleChapterIndex = (index) => {
@@ -30,6 +34,7 @@ const App = ({ course, getCourse }) => {
       setPageIndex(index);
     }
   };
+
   const initialCourse = course;
 
   if (initialCourse && chapterIndex > -1) {
@@ -45,13 +50,22 @@ const App = ({ course, getCourse }) => {
           setChapterIndex={handleChapterIndex}
           setHeaderTitle={setHeaderTitle}
         />
-        <Player
-          course={initialCourse}
-          chapterIndex={chapterIndex}
-          pageIndex={pageIndex}
-          setChapterIndex={handleChapterIndex}
-          setHeaderTitle={setHeaderTitle}
-        />
+
+        {!isLoading ? (
+          <Player
+            course={initialCourse}
+            chapterIndex={chapterIndex}
+            pageIndex={pageIndex}
+            setChapterIndex={handleChapterIndex}
+            setHeaderTitle={setHeaderTitle}
+          />
+        ) : (
+          <div className="player-wrapper">
+            <div className="flex justify-center mt-24">
+              <Loader color="#00BFFF" height={80} width={80} />
+            </div>
+          </div>
+        )}
         {chapterIndex > -1 ? (
           <FooterNav
             pageIndex={pageIndex}
@@ -67,15 +81,18 @@ const App = ({ course, getCourse }) => {
   );
 };
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   const {
-    course: { course },
+    course: { course }
   } = state;
   return { course };
 };
 
-const mapDispatchToProps = (dispatch) => ({
-  getCourse: () => dispatch(getCourseRequest()),
+const mapDispatchToProps = dispatch => ({
+  getCourse: () => dispatch(getCourseRequest())
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
