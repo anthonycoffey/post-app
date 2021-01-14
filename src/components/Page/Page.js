be792d88f6a4ed1dcd6129433bb69eb98c909dac
Elite Developer <elite.developer1055@gmail.com>
2021-01-14 17:30:39 +0800

    add test drag and drop

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -12,8 +12,8 @@ import Image from "./components/Image";
 import Text from "./components/Text";
 import List from "./components/List";
 import CustomButton from "./components/CustomButton";
+import TestDragDrop from "./components/TestDragDrop";
 import ConversationRater from "../../components/ConversationRater/ConversationRater";
-import DragAndDrop from "../../components/DragAndDrop/DragAndDrop";
 const Page = ({ elements, style, classNames }) => {
   const pageIndex = useSelector((state) => state.status.pageIndex);
   const chapterIndex = useSelector((state) => state.status.chapterIndex);
@@ -73,7 +73,8 @@ const Page = ({ elements, style, classNames }) => {
           if (element.activity === "ConversationRater") {
             return <ConversationRater data={element.data} key={index} />;
           } else if (element.activity === 'DragAndDrop') {
-            return <DragAndDrop data={element.data} key={index} />;
+            // return <DragAndDrop data={element.data} key={index} />;
+            return <TestDragDrop data={element.data} key={index} />;
           }
         } else if (element.type === "text") {
           return <Text data={element} key={index} />;
